a0b47bb1e4aa7fa7bc2cf85c045c945c6f85efef
Jan Venekamp <jan@venekamp.net>
2017-08-27 19:48:03 +0200

    Add appstream-glib

---
+++ appstream-glib.rb
@@ -0,0 +1,25 @@
+class AppStreamGlib < Formula
+  homepage "https://github.com/hughsie/appstream-glib"
+  url "https://github.com/hughsie/appstream-glib/archive/appstream_glib_0_7_2.tar.gz"
+  sha256 "80231ffc8db3a470722a5ceb85c933260649636d3a4355b075a76e6c8803b5ba"
+
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "libtool" => :build
+  depends_on "glib" => :build
+  depends_on "docbook" => :build
+  depends_on "gtk-doc" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "rpm" => :build
+  depends_on "gtk3" => :build
+  depends_on "sqlite" => :build
+  depends_on "libsoup" => :build
+  depends_on "gettext" => :build
+  depends_on "libarchive" => :build
+  depends_on "libyaml" => :build
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
--- transmission-remote-gtk.rb
+++ transmission-remote-gtk.rb
@@ -1,15 +1,20 @@
 class TransmissionRemoteGtk < Formula
   homepage "https://code.google.com/p/transmission-remote-gtk/"
-  url "https://transmission-remote-gtk.googlecode.com/files/transmission-remote-gtk-1.1.1.tar.gz"
-  sha256 "9f5ac2de623d633bcaacb6362bb6d8fb468c6dff278b867f65809c48ea1570c9"
+  url "https://github.com/transmission-remote-gtk/transmission-remote-gtk/archive/1.3.1.tar.gz"
+  sha256 "6344a4c4dd455f68030c96b5491cc6f36e9d0e2f104e2f27666fd2d4f9beab34"
 
   depends_on :x11
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "autoconf-archive" => :build
+  depends_on "appstream-glib" => :build
   depends_on "gtk+3"
   depends_on "json-glib"
 
   def install
+    system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}", "--enable-gtk3"
     system "make", "install"
   end
