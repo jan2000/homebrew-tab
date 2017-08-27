7d61c965a274a55b5c00bf2efe99e271774502fe
Jan Venekamp <jan@venekamp.net>
2017-08-27 19:11:44 +0200

    Fix transmission-remote-gtk

--- transmission-remote-gtk.rb
+++ transmission-remote-gtk.rb
@@ -8,6 +8,7 @@ class TransmissionRemoteGtk < Formula
   depends_on "pkg-config" => :build
   depends_on "automake" => :build
   depends_on "autoconf" => :build
+  depends_on "autoconf-archive" => :build
   depends_on "gtk+3"
   depends_on "json-glib"
 
