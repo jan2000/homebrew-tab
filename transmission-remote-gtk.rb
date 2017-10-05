c1302c205c6a041fb37458a4f2b8b50aaa1c1b17
Jan Venekamp <jan@venekamp.net>
2017-10-05 09:00:57 +0200

    Fix transmission-remote-gtk

--- transmission-remote-gtk.rb
+++ transmission-remote-gtk.rb
@@ -15,6 +15,7 @@ class TransmissionRemoteGtk < Formula
   depends_on "libtool" => :build
 
   def install
+    system "alias", "libtoolize=glibtoolize""
     system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}", "--enable-gtk3"
     system "make", "install"
